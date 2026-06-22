a561fe4627e36a0ef859e1dc04549536e667a8bb
Reggiws <Reggiws@users.noreply.github.com>
2026-06-22 12:26:07 +0000

    [ci] deploy site 25c43cfb27d8f36f0d2687f398cf805784d31207

--- assets/use-pagetitle-CFAlsuUP.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-BHNUuo-F.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
---
+++ assets/use-pagetitle-Cs9Sv2ql.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-ChU0cjo3.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
